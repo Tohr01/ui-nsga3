b643e1c0fa64c4de655ee9cc5fcd09b0fd31c8ff
Tohr01 <carl@Mac-mini-von-Carl.fritz.box>
2026-05-02 02:36:23 +0200

    fix: auto clear cache on new canvas dim set; max iter in while loop

--- src/scoring/debug/target_font_size.py
+++ src/scoring/debug/target_font_size.py
@@ -4,12 +4,23 @@ from ui.container import Container
 
 
 class TargetFontSizeScorer(Scorer):
-    TARGET_FONT_SIZE_PX = 60
+    """
+    Scores a container based on how close the font size of its text elements
+    are to a predifined target font size.
+    """
+
+    target_font_size_px: float
+
+    def __init__(self, target_font_size_px: float = 16):
+        """
+        :param target_font_size_px: The target font size in pixels. The scorer will penalize font sizes that are further away from this target.
+        """
+        self.target_font_size_px = target_font_size_px
 
     def score(self, container: Container) -> float:
         penalty = 0
         for element in container.elements:
             if isinstance(element, SingleLineText):
-                penalty += abs(element.font_size - self.TARGET_FONT_SIZE_PX)
+                penalty += abs(element.font_size - self.target_font_size_px)
 
         return penalty
