4e8de00563fa7f3472fa3e1aa1ed269f02cb578f
Tohr01 <carl@Mac-mini-von-Carl.local>
2026-06-26 01:48:58 +0200

    refactor: remove weighing from internal generation / individual evaluation

--- src/optimization/nsga3/problem.py
+++ src/optimization/nsga3/problem.py
@@ -5,32 +5,33 @@ from scoring.scorer import Scorer
 
 
 class ContainerProblem(Problem):
-    scorers: list[tuple[Scorer, float]]
+    scorers: list[Scorer]
     constraints: list[Scorer]
 
     def __init__(
         self,
-        scorers: list[tuple[Scorer, float]],
+        scorers: list[Scorer],
         constraints: list[Scorer],
     ):
         # We set n_var to 1 because we will pass a single Container object as the variable to optimize
         super().__init__(n_var=1, n_obj=len(scorers), n_ieq_constr=len(constraints))
         # Init scorers
-        self.scorers = [(scorer, weight) for scorer, weight in scorers]
+        self.scorers = scorers
         self.constraints = constraints
 
     def _evaluate(self, x, out, *args, **kwargs):
         objectives = []
         constraints = []
         for container in x[:, 0]:
-            scores = [
-                scorer.score(container) * weight for scorer, weight in self.scorers
-            ]
+            # Eval objective functions
+            scores = [scorer.score(container) for scorer in self.scorers]
             objectives.append(scores)
 
+            # Eval ieq constraint functions
             container_constraint_violations = [
                 constraint.score(container) for constraint in self.constraints
             ]
             constraints.append(container_constraint_violations)
+
         out["F"] = np.array(objectives, dtype=float)
         out["G"] = np.array(constraints, dtype=float)
